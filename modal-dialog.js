daab7eec22a4353e30db3ac458abeb6d6ecb008a
Denis Baskovsky <baskovskiy@bifit.com>
2016-01-14 17:42:53 +0300

    init

---
+++ modal-dialog.js
@@ -0,0 +1,53 @@
+'use strict';
+
+new Polymer({
+  is: 'modal-dialog',
+
+  properties: {
+    content: String,
+
+    dismissText: {
+      type: String,
+      value() {
+        return 'ОТМЕНА';
+      }
+    },
+    confirmText: {
+      type: String,
+      value () {
+        return 'ОТКЛЮЧИТЬ';
+      }
+    }
+  },
+
+  /**
+   * Callback
+   */
+  onConfirm () {
+    console.warn('override this!');
+  },
+
+  /**
+   * Установить контент
+   */
+  setContent (text) {
+    this.content = text.trim();
+  },
+
+  /**
+   * Открыть модальное окно
+   */
+  open (onConfirmCallback) {
+    this.$$('[modal]').open();
+
+    this.onConfirm = onConfirmCallback;
+  },
+
+  /**
+   * Закрыть модальное окно
+   */
+  close() {
+    this.$$('[modal]').close();
+  }
+
+});
